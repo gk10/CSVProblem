e1bbf2317fce6273f9765e66b3013b1b42c3e318
Greg Kline <gkline@stetson.edu>
2021-06-15 02:42:33 -0400

    comments; misc

--- src/Main.java
+++ src/Main.java
@@ -15,7 +15,8 @@ public class Main {
 	 * using what can be found in the java standard
 	 */
 	public static void main(String[] args) {
-		// I'm assuming the the data columns are in order of: ID, first name, last name, version, and company
+		// I'm assuming the the data columns are in order of: ID, first name, last name,
+		// version, and company
 		String file = "Path\\To\\.csv";
 		try {
 			sortCustomers(cleanData(csvData(file)));
@@ -25,26 +26,33 @@ public class Main {
 		}
 	}
 
+	/**
+	 * Read in file and return an ArrayList of csv rows
+	 * @param file path to .csv file
+	 * @return arraylist of .csv file rows
+	 * @throws IOException
+	 */
 	public static ArrayList<String> csvData(String file) throws IOException {
 		ArrayList<String> data = new ArrayList<String>();
 		BufferedReader br = new BufferedReader(new FileReader(file));
 		String line;
 
 		while ((line = br.readLine()) != null) {
-			System.out.println(line);
 			data.add(line);
 		}
-		System.out.println(data.size());
 		return data;
 
 	}
 
+	/**
+	 * 
+	 * Convert ArrayList of comma separated strings into ArrayList of customers
+	 * 
+	 * @param data ArrayList of comma separated strings
+	 * @return ArrayList now of type Customer; removes header row
+	 */
 	public static ArrayList<Customers> cleanData(ArrayList<String> data) {
-		// List<String> cats = Arrays.asList(data.get(0).split(","));
 		ArrayList<Customers> customers = new ArrayList<Customers>();
-		// for (String temp : cats) {
-		// System.out.println(temp);
-		// }
 		for (int i = 1; i < data.size(); i++) {
 			List<String> temp = Arrays.asList(data.get(i).split(","));
 			Customers current = new Customers();
@@ -52,13 +60,19 @@ public class Main {
 			current.setFname(temp.get(1));
 			current.setlName(temp.get(2));
 			current.setVersion(Integer.parseInt(temp.get(3)));
-			current.setID(temp.get(4));
+			current.setCompany(temp.get(4));
 			customers.add(current);
 		}
 		return customers;
 	}
 
-	public static void sortCustomers(ArrayList<Customers> data) {
+	/**
+	 * Separates customers by their company; 
+	 * Sorts by last and first name asc
+	 * Remove duplicate IDs within company and retain one with highest version
+	 * @param data ArrayList of customers
+	 */
+	public static HashMap<String, ArrayList<Customers>> sortCustomers(ArrayList<Customers> data) {
 		HashMap<String, ArrayList<Customers>> map = new HashMap<>();
 		// map customers to their correct company
 		for (int i = 0; i < data.size(); i++) {
@@ -74,25 +88,42 @@ public class Main {
 				for (int j = i + 1; j < curCompany.size(); j++) {
 					Customers two = curCompany.get(j);
 					if (one.getID().equals(two.getID())) {
+						
 						if (one.getVersion() > two.getVersion()) {
 							curCompany.remove(j);
 						} else {
 							curCompany.remove(i);
 						}
+						i = 0;
+						j = 0;
 					}
-				}				
+				}
 			}
-			
-			
-			
+
 			// overwrite old unsorted arraylist with new sorted arraylist
 			Collections.sort(curCompany, ascOrder);
-			map.put(key, curCompany);			
+			map.put(key, curCompany);
+
+			// debug loop
+//			for (int i = 0; i < curCompany.size(); i++) {
+//				System.out.println(curCompany.get(i).getID() + ", " + curCompany.get(i).getFname() + ", "
+//						+ curCompany.get(i).getLname() + ", " + curCompany.get(i).getCompany() + ", "
+//						+ curCompany.get(i).getVersion());
+//			}
+//			System.out.println("");
 		}
+		return map;
+
 	}
+
 	public static Comparator<Customers> ascOrder = new Comparator<Customers>() {
-	public int compare(Customers a, Customers b) {
-		return a.getLname().compareTo(b.getLname());
-	}
+		public int compare(Customers a, Customers b) {
+			if (a.getLname().compareTo(b.getLname()) == 0) {
+				return a.getFname().compareTo(b.getFname());
+			} else {
+				return a.getLname().compareTo(b.getLname());
+			}
+		}
 	};
+
 }
